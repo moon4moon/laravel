5e666c4973b47a2ee3227e480753e94ce1857025
moon4mooon <grzegorz.kafel11@gmail.com>
2023-05-16 19:00:47 +0200

    save

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $now = Carbon::now();
+
+        $faker = Faker::create();
+
+        $people = array();
+
+        for($i = 0; $i < 200; $i++) {
+            $people[] = [
+                'name' => $faker -> firstName,
+                'surname' => $faker -> lastName,
+                'phone_number' => $faker -> phoneNumber,
+                'street' => $faker -> streetName,
+                'city' => $faker -> city,
+                'country' => $faker -> country,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        DB::table('people')->instert($people);
     }
 }
